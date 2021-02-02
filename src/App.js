604a9762f66eb4614aa76cf8cd30705d51e03b8e
Alex_Kelly <afkelly14@Gmail.com>
2021-02-02 13:58:30 -0500

    changing background color based on temp

--- src/App.js
+++ src/App.js
@@ -59,13 +59,12 @@ function App() {
   return (
     <div
       className={
-        typeof weather.main != "undefined"
-          ? weather.main.temp > 16
+        (typeof weather.main != "undefined")
+          ? ((weather.main.temp > 16)
             ? "app warm"
-            : "app"
+            : "app")
           : "app"
-      }
-    >
+          }>
       <main>
         <div className="search-box">
           <input
@@ -86,7 +85,7 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}</div>
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
