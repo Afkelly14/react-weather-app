b7fff90ff77dae749a10e6fff2f0f842e4210a7e
Alex_Kelly <afkelly14@Gmail.com>
2021-02-02 14:12:50 -0500

    change metrics

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ const api = {
 };
 
 function App() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
   //search for new cities weather
   const search = (evt) => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
@@ -59,12 +59,13 @@ function App() {
   return (
     <div
       className={
-        (typeof weather.main != "undefined")
-          ? ((weather.main.temp > 16)
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 16
             ? "app warm"
-            : "app")
+            : "app"
           : "app"
-          }>
+      }
+    >
       <main>
         <div className="search-box">
           <input
@@ -85,7 +86,7 @@ function App() {
               <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="temp">{Math.round(weather.main.temp)}°</div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
